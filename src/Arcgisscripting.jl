cea4be9c25bdf3d44c81e77d695cb11629c80829
ranjanan <benditlikeranjan@gmail.com>
2018-05-26 12:32:03 -0400

    Add a couple of simple methods

--- src/Arcgisscripting.jl
+++ src/Arcgisscripting.jl
@@ -3,7 +3,15 @@ module Arcgisscripting
 using PyCall
 @pyimport arcgisscripting as ags
 
-create() = ags.create()
+struct GeoProcessorObject
+	o::PyObject
+end
+create() = ags.create() |> GeoProcessorObject
+
+for f in (:CheckOutExtension, :SingleOutputAlgebra_sa)
+	@eval ($f)(g::GeoProcessorObject, args...) = g.o[f](args...)
+end
+
 
 
 end # module
